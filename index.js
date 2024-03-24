1ea93e87e7a32dee0ed91bc633b02beee8152243
Ahsan Khan <afzalahsankhan283@gmail.com>
2024-03-24 20:52:38 +0500

    code
    code

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "UsersguessedNumber",
+        type: "number",
+        message: "PLEASE GUESS A NUMBER between 1-10",
+    },
+]);
+if (answer.UsersguessedNumber === randomNumber) {
+    console.log("congrats! you guessed correct number🎉👏");
+}
+else {
+    console.log("you guessed wrong number 😢");
+}
